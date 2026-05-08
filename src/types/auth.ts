d1997eb449f6ba02b3aed1a7bacb2bbd2c19aaeb
ywyh <yassienwyh0@gmail.com>
2026-05-08 20:33:32 +0300

    fix: zod schemas

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -2,13 +2,17 @@ import { z } from "zod";
 
 export const usernameSchema = z
   .string()
-  .min(3, "Username should at least be 3 characters.");
+  .min(3, "Username should at least be 3 characters.")
+  .regex(/^[a-zA-Z0-9]+$/, "Username can only contain letters and numbers")
+  .transform((val) => val.toLowerCase());
+
 export const emailSchema = z
   .email()
-  .min(3, "Email should at least be 3 characters.");
+  .min(2, "Email should at least be 2 characters.")
+  .transform((val) => val.toLowerCase());
 export const passwordSchema = z
   .string()
-  .min(8, "Password should at least be 8 characters.");
+  .min(4, "Password should at least be 4 characters.");
 export const identifierSchema = z
   .string()
   .min(3, "Identifier should at least be 3 characters.");
